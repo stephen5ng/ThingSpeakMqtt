70e649f70a7d837b8ac14f9d684c1a600536c46d
stephen5ng <110236404+stephen5ng@users.noreply.github.com>
2022-10-26 11:29:58 -0700

    Add ThingSpeakMqtt.ino.
    Based on https://www.envirodiy.org/topic/connecting-sim7080-to-new-thingspeak/#post-16247.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ThingSpeakMqtt
+
+platformio.ini should look something like this:
+
+    [env:mayfly]
+    monitor_speed = 115200
+    board = mayfly
+    platform = atmelavr
+    framework = arduino
+    lib_ignore = RTCZero
+    build_flags =
+        -D MQTT_USERNAME=YOUR_USERNAME
+        -D MQTT_PASSWORD=YOUR_PASSWORD
+        -D MQTT_CLIENT_ID=YOUR_CLIENT_ID
+        -D THINGSPEAK_CHANNEL_ID='"1851144"'
+        -DSDI12_EXTERNAL_PCINT
+        ; DEBUG OPTIONS:
+        ; -DNEOSWSERIAL_EXTERNAL_PCINT
+        ; -D STANDARD_SERIAL_OUTPUT=Serial
+        ; -D DEBUGGING_SERIAL_OUTPUT=Serial
+        ; -D DEEP_DEBUGGING_SERIAL_OUTPUT=Serial    
+        ; -D MS_LOGGERBASE_DEBUG
+        ; https://github.com/EnviroDIY/ModularSensors/commit/cc9a66edb4d0576c6e3f0d15e908876d30224879
+        ; -D MS_DEBUGGING_STD
+    lib_deps =
+        EnviroDIY_ModularSensors
+        vshymanskyy/TinyGSM@^0.11.5
---
+++ ThingSpeakMqtt.ino
@@ -0,0 +1,195 @@
+#include "GsmClient.h"
+#include <Arduino.h>
+#include <EnableInterrupt.h>
+#include <ModularSensors.h>
+#include <modems/SIMComSIM7080.h>
+#include <sensors/ProcessorStats.h>
+#include <sensors/MaximDS3231.h>
+#include "ThingSpeakPublisher.h"
+
+// Note:  Please change these battery voltages to match your battery
+#define BATTERY_VOLTAGE_LOW 3.4
+#define BATTERY_VOLTAGE_MODERATE 3.55
+
+const char *LoggerID = "logger"; // Logger ID and prefix for the name of the data file on SD card
+const uint8_t loggingIntervalMinutes = 1;
+
+// NOTE:  Daylight savings time will not be applied!  Please use standard time!
+const int8_t timeZone = -8; // Pacific Standard Time
+
+// NOTE:  Use -1 for pins that do not apply
+const int32_t serialBaud = 115200;
+const int8_t greenLED = 8;
+const int8_t redLED = 9;
+const int8_t buttonPin = 21;
+const int8_t wakePin = 31; // MCU interrupt/alarm pin to wake from sleep
+// Mayfly 0.x D31 = A7
+// Set the wake pin to -1 if you do not want the main processor to sleep.
+// In a SAMD system where you are using the built-in rtc, set wakePin to 1
+
+const int8_t sdCardPwrPin = -1;   // MCU SD card power pin
+const int8_t sdCardSSPin = 12;    // SD card chip select/slave select pin
+const int8_t sensorPowerPin = 22; // MCU pin controlling main sensor power
+
+#define modemSerial Serial1
+
+const int32_t modemBaud = 9600; //  SIM7080 does auto-bauding by default, but for simplicity we set to 9600
+
+// Modem Pins - Describe the physical pin connection of your modem to your board
+// NOTE:  Use -1 for pins that do not apply
+
+const int8_t modemVccPin = 18;     // MCU pin controlling modem power --- Pin 18 is the power enable pin for the bee socket on Mayfly v1.0,
+                                   //  use -1 if using Mayfly 0.5b or if the bee socket is constantly powered (ie you changed SJ18 on Mayfly1.0 to 3.3v)
+const int8_t modemStatusPin = 19;  // MCU pin used to read modem status
+const int8_t modemSleepRqPin = 23; // MCU pin for modem sleep/wake request
+const int8_t modemLEDPin = redLED; // MCU pin connected an LED to show modem status
+
+const char *apn = "hologram"; // APN connection name, typically Hologram unless you have a different provider's SIM card. Change as needed
+
+SIMComSIM7080 modem7080(&modemSerial, modemVccPin, modemStatusPin,
+                        modemSleepRqPin, apn);
+SIMComSIM7080 modem = modem7080;
+
+// Create the main processor chip "sensor" - for general metadata
+const char *mcuBoardVersion = "v0.5b"; // is only used for onboard battery voltage calculation,
+                                       // "v0.5b" is safe to use on Mayfly v1.0 boards because the formula is the same
+ProcessorStats mcuBoard(mcuBoardVersion);
+
+MaximDS3231 ds3231(1);
+
+Variable *variableList[] = {
+    new ProcessorStats_Battery(&mcuBoard),
+};
+int variableCount = sizeof(variableList) / sizeof(variableList[0]);
+VariableArray varArray;
+
+Logger dataLogger(LoggerID, loggingIntervalMinutes, &varArray);
+
+ThingSpeakPublisher TsMqtt;
+
+void greenredflash(uint8_t numFlash = 4, uint8_t rate = 75)
+{
+    for (uint8_t i = 0; i < numFlash; i++)
+    {
+        digitalWrite(greenLED, HIGH);
+        digitalWrite(redLED, LOW);
+        delay(rate);
+        digitalWrite(greenLED, LOW);
+        digitalWrite(redLED, HIGH);
+        delay(rate);
+    }
+    digitalWrite(redLED, LOW);
+}
+
+// Reads the battery voltage
+// NOTE: This will actually return the battery level from the previous update!
+float getBatteryVoltage()
+{
+    if (mcuBoard.sensorValues[0] == -9999)
+        mcuBoard.update();
+    return mcuBoard.sensorValues[0];
+}
+
+void setup()
+{
+    // Start the primary serial connection
+    Serial.begin(serialBaud);
+
+    // Print a start-up note to the first serial port
+    Serial.print(F("Now running "));
+    Serial.print(F(" on Logger "));
+    Serial.println(LoggerID);
+    Serial.println();
+
+    Serial.print(F("Using ModularSensors Library version "));
+    Serial.println(MODULAR_SENSORS_VERSION);
+    Serial.print(F("TinyGSM Library version "));
+    Serial.println(TINYGSM_VERSION);
+    Serial.println();
+
+    // Start the serial connection with the modem
+    modemSerial.begin(modemBaud);
+
+    pinMode(greenLED, OUTPUT);
+    digitalWrite(greenLED, LOW);
+    pinMode(redLED, OUTPUT);
+    digitalWrite(redLED, LOW);
+    greenredflash(); // Blink the LEDs to show the board is on and starting up
+
+    pinMode(20, OUTPUT); // for proper operation of the onboard flash memory chip's ChipSelect (Mayfly v1.0 and later)
+
+    Logger::setLoggerTimeZone(timeZone);
+    // It is STRONGLY RECOMMENDED that you set the RTC to be in UTC (UTC+0)
+    Logger::setRTCTimeZone(0);
+
+    dataLogger.attachModem(modem);
+    modem.setModemLED(modemLEDPin);
+    dataLogger.setLoggerPins(wakePin, sdCardSSPin, sdCardPwrPin, buttonPin,
+                             greenLED);
+
+    varArray.begin(variableCount, variableList);
+    dataLogger.begin(LoggerID, loggingIntervalMinutes, &varArray);
+    TsMqtt.begin(dataLogger, &modem.gsmClient,
+                 THINGSPEAK_CHANNEL_ID,
+                 MQTT_USERNAME,
+                 MQTT_PASSWORD,
+                 MQTT_CLIENT_ID);
+
+    if (getBatteryVoltage() > BATTERY_VOLTAGE_LOW)
+    {
+        Serial.println(F("Setting up sensors..."));
+        varArray.setupSensors();
+    }
+
+    modem.setModemWakeLevel(HIGH);  // ModuleFun Bee inverts the signal
+    modem.setModemResetLevel(HIGH); // ModuleFun Bee inverts the signal
+    Serial.println(F("Waking modem and setting Cellular Carrier Options..."));
+    modem.modemWake();                 // NOTE:  This will also set up the modem
+    modem.gsmModem.setBaud(modemBaud); // Make sure we're *NOT* auto-bauding!
+    modem.gsmModem.setNetworkMode(NETWORK_MODE_GSM_ONLY);
+    modem.gsmModem.setPreferredMode(PREFERRED_MODE_CAT_M);
+
+    // Sync the clock if it isn't valid or we have battery to spare
+    if (getBatteryVoltage() > BATTERY_VOLTAGE_MODERATE || !dataLogger.isRTCSane())
+    {
+        // Synchronize the RTC with NIST
+        // This will also set up the modem
+        dataLogger.syncRTC();
+    }
+
+    // Create the log file, adding the default header to it
+    // Do this last so we have the best chance of getting the time correct and
+    // all sensor names correct
+    // Writing to the SD card can be power intensive, so if we're skipping
+    // the sensor setup we'll skip this too.
+    if (getBatteryVoltage() > BATTERY_VOLTAGE_LOW)
+    {
+        Serial.println(F("Setting up file on SD card"));
+        dataLogger.turnOnSDcard(true);  // wait for card to settle after power up
+        dataLogger.createLogFile(true); // write a new header
+        dataLogger.turnOffSDcard(true); // wait for internal housekeeping after write
+    }
+
+    Serial.println(F("Putting processor to sleep\n"));
+    dataLogger.systemSleep();
+}
+
+void loop()
+{
+    // At very low battery, just go back to sleep
+    if (getBatteryVoltage() < BATTERY_VOLTAGE_LOW)
+    {
+        dataLogger.systemSleep();
+    }
+    // At moderate voltage, log data but don't send it over the modem
+    else if (getBatteryVoltage() < BATTERY_VOLTAGE_MODERATE)
+    {
+        dataLogger.logData();
+    }
+    // If the battery is good, send the data to the world
+    else
+    {
+        Serial.println(F("dataLogger.logDataAndPublish()\n"));
+        dataLogger.logDataAndPublish();
+    }
+}
